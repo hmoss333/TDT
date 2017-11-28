2050e5f36c561756e15e0e7140cee1f59e442048
hmoss333 <hdamoss@gmail.com>
2017-11-28 14:02:32 -0500

    Update 11/28/2017
    -Implimented player controls
    -Implimented raycasting
    -Player can check directly in front of them based on last input
    -Created prefabs for player and obstacles

---
+++ Assets/Scripts/Player/Player_Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+
+    public float speed;
+    public float checkDist;
+
+    float xInput;
+    float yInput;
+
+    Rigidbody2D rb2d;
+
+    Vector3 previousGood = Vector3.zero;
+    RaycastHit2D foundHit;
+
+    // Use this for initialization
+    void Start () {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        xInput = Input.GetAxisRaw("Horizontal");
+        yInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = new Vector2(xInput, yInput) * speed;
+        rb2d.velocity = dir;
+
+        if (dir == Vector3.zero)
+        {
+            dir = previousGood;
+        }
+        else
+        {
+            previousGood = dir;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            foundHit = Physics2D.Raycast(transform.position, dir, checkDist, 1 << LayerMask.NameToLayer("Wall"));
+            Debug.DrawRay(transform.position, dir, Color.green);
+
+            if (foundHit.collider != null)
+            {
+                Debug.Log("Looking at a thing: " + foundHit.transform.name);
+            }
+        }
+    }
+}
