f0208b95650b25cd425918aa2c47a7ca39f01338
hmoss333 <hdamoss@gmail.com>
2017-12-08 10:44:47 -0500

    Update 12/8/2017 pt.2
    -Added sprites
    -Setup animations for NPCs
    -Added random NPC rotations or set direction
    -Added pickup object

---
+++ Assets/Scripts/Objects/NPC_Rotate.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Rotate : MonoBehaviour {
+
+	public enum LookDirection { up, left, right, down }
+    public LookDirection lookDir;
+
+    public bool isRandom;
+
+    Animator anim;
+    
+    // Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        Coroutine co = StartCoroutine(SetRotation());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator SetRotation ()
+    {
+        switch (lookDir)
+        {
+            case LookDirection.up:
+                anim.SetTrigger("LookUp");
+                break;
+            case LookDirection.left:
+                anim.SetTrigger("LookLeft");
+                break;
+            case LookDirection.right:
+                anim.SetTrigger("LookRight");
+                break;
+            case LookDirection.down:
+                anim.SetTrigger("LookDown");
+                break;
+            default:
+                anim.SetTrigger("LookDown");
+                break;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        if (isRandom)
+        {
+            int randNum = Random.Range(0, 4);
+
+            switch (randNum)
+            {
+                case 0:
+                    //anim.SetTrigger("LookUp");
+                    lookDir = LookDirection.up;
+                    break;
+                case 1:
+                    //anim.SetTrigger("LookLeft");
+                    lookDir = LookDirection.left;
+                    break;
+                case 2:
+                    //anim.SetTrigger("LookRight");
+                    lookDir = LookDirection.right;
+                    break;
+                case 3:
+                    //anim.SetTrigger("LookDown");
+                    lookDir = LookDirection.down;
+                    break;
+                default:
+                    lookDir = LookDirection.down;
+                    break;
+            }
+
+            Coroutine co = StartCoroutine(SetRotation());
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/Object_PickUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_PickUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            //increment value or add to inventory
+            Debug.Log("Picked up item");
+            Destroy(this.gameObject);
+        }
+    }
+}
